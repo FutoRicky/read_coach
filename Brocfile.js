95cdfb257f49c4db034f59f1170ec910eca2041c
futoricky <ricardoamercado@gmail.com>
2015-10-09 23:38:39 -0400

    get flipclock working in component

--- Brocfile.js
+++ Brocfile.js
@@ -17,8 +17,10 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-app.import('bower_components/bootstrap/dist/js/bootstrap.js');
 app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+app.import('bower_components/FlipClock/compiled/flipclock.css');
+app.import('bower_components/FlipClock/compiled/flipclock.js');
 app.import('app/styles/main-page.css');
 app.import('app/styles/nav-bar.css');
 
